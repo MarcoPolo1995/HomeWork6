65cdcf1b5af730cac8968968334113a6f4204401
marco_polo <nol-95@mail.ru>
2021-07-09 12:28:08 +0300

    ДЗ ПОСЛЕ 6 УРОКА

--- src/HomeWork6/MainClass.java
+++ src/HomeWork6/MainClass.java
@@ -2,7 +2,23 @@ package HomeWork6;
 
 public class MainClass {
     public static void main(String[] args) {
-        
+        Animals animal = new Animals("Cats");
+        Cat cat = new Cat("Barsik", 200, false);
+        animal.animalInfo();
+        cat.animalInfo();
+        cat.catInfo();
+
+        Animals danimal = new Animals("Dogs");
+        Dog dog = new Dog("Sharik", 500, 10);
+        danimal.animalInfo();
+        dog.animalInfo();
+        dog.dogInfo();
+
     }
 
-}
+    }
+
+
+
+
+
